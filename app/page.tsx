2eadbfac4af96683973058d2a42bd66d0b9efe73
Yasiel Cabrera <yasiel9506@gmail.com>
2023-09-26 11:47:19 -0400

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,42 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import styles from "./page.module.css";
+import {
+  AccountsSnapshot,
+  AccountsSnapshotSkeleton,
+  ResourceType,
+  ThemeProvider,
+} from "@yasiel.cabrera/ses-snapshots-library";
+import { useEffect, useState } from "react";
+import { snapshot_mock } from "./mock";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLight, setIsLight] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 5000);
+  }, []);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div>
+        <button onClick={() => setIsLight(!isLight)}>
+          Toggle Theme: {isLight ? "Light" : "Dark"}
+        </button>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <ThemeProvider isLight={isLight}>
+        {isLoading ? (
+          <AccountsSnapshotSkeleton />
+        ) : (
+          <AccountsSnapshot
+            resourceType={ResourceType.EcosystemActor}
+            sinceDate={new Date("2021-01-01")}
+            snapshotOwner="Mock Ecosystem Actor"
+            snapshot={snapshot_mock}
+          />
+        )}
+      </ThemeProvider>
     </main>
-  )
+  );
 }
